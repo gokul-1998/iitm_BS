bcf01be8d15fd013dd74b6c90dc6ff1fb2915079
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-17 08:32:03 +0530

    refactor authentication tests; update client references and improve code consistency

--- tests/functional/test_add_user.py
+++ tests/functional/test_add_user.py
@@ -1,31 +1,28 @@
-
-
-
-
-
-def test_auth_with_new_machine(client, init_data):
-    init_data(username='test_user', password='test_pass')
+def test_auth_with_new_machine(test_client, init_data):
+    init_data(username="test_user", password="test_pass")
     headers = {
-        'SEEK_CUSTOM_AUTH': 'test_user:test_pass:' + 'a' * 64
+        "SEEK_CUSTOM_AUTH": "test_user:test_pass:" + "a" * 64
     }
-    response = client.get('/check', headers=headers)
+    response = test_client.get("/check", headers=headers)
     assert response.status_code == 201
-    assert response.json['message'] == 'Machine added and authentication successful.'
+    assert response.json["message"] == "Machine added and authentication successful."
 
-def test_auth_with_existing_machine(client, init_data):
-    user = init_data(username='test_user', password='test_pass', machine_id='a' * 64)
+
+def test_auth_with_existing_machine(test_client, init_data):
+    user = init_data(username="test_user", password="test_pass", machine_id="a" * 64)
     headers = {
-        'SEEK_CUSTOM_AUTH': f'test_user:test_pass:{user.machines[0].machine_id}'
+        "SEEK_CUSTOM_AUTH": f"test_user:test_pass:{user.machines[0].machine_id}"
     }
-    response = client.get('/check', headers=headers)
+    response = test_client.get("/check", headers=headers)
     assert response.status_code == 200
-    assert response.json['message'] == 'Authentication successful.'
+    assert response.json["message"] == "Authentication successful."
 
-def test_invalid_credentials(client, init_data):
-    init_data(username='test_user', password='test_pass')
+
+def test_invalid_credentials(test_client, init_data):
+    init_data(username="test_user", password="test_pass")
     headers = {
-        'SEEK_CUSTOM_AUTH': 'test_user:wrong_pass:' + 'a' * 64
+        "SEEK_CUSTOM_AUTH": "test_user:wrong_pass:" + "a" * 64
     }
-    response = client.get('/check', headers=headers)
+    response = test_client.get("/check", headers=headers)
     assert response.status_code == 401
-    assert response.json['message'] == 'Invalid username or password.'
+    assert response.json["message"] == "Invalid username or password."
